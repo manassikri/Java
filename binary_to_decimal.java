4de01217219fb02654dce741c45df6abe1658b62
Manas Sikri <manassikri04@gmail.com>
2019-02-21 23:36:21 +0530

    Update binary_to_decimal.java

--- binary_to_decimal.java
+++ binary_to_decimal.java
@@ -4,19 +4,23 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-		
-		int bin = scn.nextInt();
-		int dec = bin2Dec(bin);
-		System.out.println(dec);
-	}
-	
-	public static int bin2Dec(int num){
-		int retVal = 0;
       
-      	/* Start your code here */
+      int bin = scn.nextInt();
+      int dec = bin2Dec(bin);
+      System.out.println(dec);
+   }
+   
+   public static int bin2Dec(int num){
+      int retVal = 0;
       
-        /* End your code here */
+           int l,r=0;
+	         while(num!=0){
+	         l=num%10;
+	         retVal=retVal+l*(int)(Math.pow(2, r));
+	         r++;
+	         num=num/10;
+	         }
 
-		return retVal;
-	}
+      return retVal;
+   }
 }
