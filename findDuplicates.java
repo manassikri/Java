e2b58bed5d7ee4deda747a9fae18ccdd4f371bec
Manas Sikri <manassikri04@gmail.com>
2020-08-07 00:44:45 +0530

    Create findDuplicates.java
    Question 442. Find All Duplicates in an Array
    Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
    
    Find all the elements that appear twice in this array.
    
    Could you do it without extra space and in O(n) runtime?
    
    Example:
    Input:
    [4,3,2,7,8,2,3,1]
    
    Output:
    [2,3]

---
+++ findDuplicates.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        
+        
+        for(int no : nums){
+            nums[Math.abs(no)-1]*=-1;
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[Math.abs(nums[i])-1]>0) {
+                ans.add(Math.abs(nums[i]));
+                nums[Math.abs(nums[i])-1]*=-1;
+            }
+        }
+        
+        return ans;
+    }
+}
